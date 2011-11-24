18878c1c15e648d75ef78744ff27f0ed85fa4259
Dave Simons <dave@inuits.be>
2011-11-25 00:33:17 +0100

    archlinux compat in puppetmaster::service class
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/service.pp
+++ manifests/classes/service.pp
@@ -2,6 +2,14 @@ class puppetmaster::service {
   service {
     'puppetmaster':
       ensure => running,
-      enable => true;
+      enable => true,
+      hasstatus => $::operatingsystem ? {
+        default => undef,
+        archlinux => false,
+      },
+      path => $::operatingsystem ? {
+        default => undef,
+        archlinux => '/etc/rc.d',
+      };
   }
 }
